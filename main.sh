edc695f47059cbdc4179e2d0215e93c8a4459797
Noirand <noirapplegray@gmail.com>
2023-08-26 07:02:09 +0900

    [update] 作成中

--- main.sh
+++ main.sh
@@ -2,11 +2,18 @@
 
 for org in original/*.m3u
 do
-	cp "$org" ./output/
+	cp "$org" ./output
+	#cp "$org" "${org%.m3u}".txt
+	#mv "${org%.m3u}".txt ./output
 	echo "$org"
 	TARGET=`echo ${org/original/output}`
+	#TARGET=`echo ${TARGET/m3u/txt}`
 	echo "$TARGET"
-	#cat "$TARGET"
+
+	# 改行コードがおかしくて cat で読めなくなっているので変換
+	nkf -Lu --overwrite "$TARGET"
+
+	sed -i -e '/#EXTINF/d' "$TARGET"
 	cat "$TARGET" | while read line						  
 	do
 		echo "${line}"
