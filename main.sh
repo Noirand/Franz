0bb81b84771263d86df47c4c1cddc628815f7df6
Noirand <noirapplegray@gmail.com>
2023-08-26 15:23:51 +0900

    [update] implement replace action

--- main.sh
+++ main.sh
@@ -13,9 +13,28 @@ do
 	# 改行コードがおかしくて cat で読めなくなっているので変換
 	nkf -Lu --overwrite "$TARGET"
 
+	# remove unnessesary lines
 	sed -i -e '/#EXTINF/d' "$TARGET"
+	
 	cat "$TARGET" | while read line						  
 	do
-		echo "${line}"
+		#echo "${line}"
+		if [ "$line" != "#EXTM3U" ]; then
+			MUSIC="${line##*/}"
+			#ALBUM="${line##*/MUSIC}"
+			ALBUM=`echo "$line" | awk -F "/" '{ print $(NF - 1) }'`
+			#ARTIST="${line##*/ALBUM}"
+			ARTIST=`echo "$line" | awk -F "/" '{ print $(NF - 2) }'`
+			echo MUSIC = "$MUSIC"
+			echo ALBUM = "$ALBUM"
+			echo ARTIST = "$ARTIST"
+
+			# relpace whole file paths for Android
+			sed -i -e "/$MUSIC\$/s/.*/$ARTIST\/$ALBUM\/$MUSIC/" "$TARGET"
+
+			# copy music file to proper folder
+			mkdir -p output/music/"$ARTIST"/"$ALBUM"
+			cp "$line" ./output/music/"$ARTIST"/"$ALBUM"
+		fi
 	done
 done
