b472a962c927cb8037f769f57d9b257651a17c63
Noirand <noirapplegray@gmail.com>
2023-08-26 15:49:06 +0900

    [remove] remove unnecessary lines

--- main.sh
+++ main.sh
@@ -3,12 +3,9 @@
 for org in original/*.m3u
 do
 	cp "$org" ./output
-	#cp "$org" "${org%.m3u}".txt
-	#mv "${org%.m3u}".txt ./output
-	echo "$org"
+	#echo "$org"
 	TARGET=`echo ${org/original/output}`
-	#TARGET=`echo ${TARGET/m3u/txt}`
-	echo "$TARGET"
+	#echo "$TARGET"
 
 	# 改行コードがおかしくて cat で読めなくなっているので変換
 	nkf -Lu --overwrite "$TARGET"
@@ -21,13 +18,8 @@ do
 		#echo "${line}"
 		if [ "$line" != "#EXTM3U" ]; then
 			MUSIC="${line##*/}"
-			#ALBUM="${line##*/MUSIC}"
 			ALBUM=`echo "$line" | awk -F "/" '{ print $(NF - 1) }'`
-			#ARTIST="${line##*/ALBUM}"
 			ARTIST=`echo "$line" | awk -F "/" '{ print $(NF - 2) }'`
-			echo MUSIC = "$MUSIC"
-			echo ALBUM = "$ALBUM"
-			echo ARTIST = "$ARTIST"
 
 			# relpace whole file paths for Android
 			sed -i -e "/$MUSIC\$/s/.*/$ARTIST\/$ALBUM\/$MUSIC/" "$TARGET"
